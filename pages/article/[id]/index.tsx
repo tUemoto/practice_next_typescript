e8542945130efcbee47fa573f76ed054e4942f62
tkpy543 <takspooh@gmail.com>
2022-05-08 01:42:54 +0900

    getStaticPathの利用方法の学習

--- pages/article/[id]/index.tsx
+++ pages/article/[id]/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next'
+import { GetServerSideProps, GetStaticPaths, GetStaticProps } from 'next'
 import Link from 'next/link'
 // import { useRouter } from 'next/router'
 const Article = ({ article }: { article: BlogPost }) => {
@@ -16,15 +16,57 @@ const Article = ({ article }: { article: BlogPost }) => {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
+/**
+ * ページのレンダリング処理の前にサーバーに問い合わせて
+ * データを取得する
+ * リクエストのたびに実行される。
+ * キャッシュが残るのが嫌な場合にはこちらを使う。
+ */
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   const res = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts/${context?.params?.id}`,
+//   )
+
+//   const article: BlogPost = await res.json()
+
+//   return {
+//     props: { article },
+//   }
+// }
+
+/**
+ * getStaticPathsを利用する際は
+ * getStaticPropsと一緒に使う。
+ * getStaticPathsは、ビルドのタイミングで実行され、ランタイムでは実行されない。（bundleしたときにこの部分はclient-sideにはコードが露出しない）
+ * getStaticPropsはそれぞれの`next build`のときに実行される
+ * fallback: trueに設定するとバックグラウンドで実行される
+ * fallback: blockingに設定すると最初のレンダリング時に実行される。
+ *
+ * ビルド時に1度だけ実行され、キャッシュの利用ができる。
+ * →DBやAPI、headless CMSやfilesystemの利用で有効
+ * ビルド時に実行するので表示が早い。
+ * SEOを気にする場合はこちらを利用する
+ */
+export const getStaticProps: GetStaticProps = async (context) => {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${context?.params?.id}`,
   )
 
   const article: BlogPost = await res.json()
-
   return {
     props: { article },
+  }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/`)
+  const articles: BlogPost[] = await res.json()
+
+  const ids = articles.map((article) => article.id)
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }))
+  return {
+    paths,
+    fallback: false,
   }
 }
 
